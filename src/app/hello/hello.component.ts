b2585476a1c8177d5bb08bce572ffb28f1dcfa43
J-Niedziolka <jn85638@stud.uph.edu.pl>
2022-11-20 17:35:53 +0100

    finished all compulsory tasks (1-7)

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,5 +1,6 @@
 import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-hello',
@@ -16,7 +17,8 @@ export class HelloComponent implements OnInit {
     {id: 4, name: "Posprzątać" },
   ];
   lastId: number = this.items.length;
-  todoItem: string = "<podaj czynność do wykonania>";
+  todoItem: string = "";
+  errorBoolean = false;
 
   constructor() { }
 
@@ -33,9 +35,25 @@ export class HelloComponent implements OnInit {
   }
 
   addItem(){
-    const item = { id: ++this.lastId, name: this.todoItem };
-    this.items.push(item);
-    this.todoItem = "";
+    const index = this.items.findIndex(obj => obj.name == this.todoItem);
+    //#2 solution
+    if(index !== -1){
+      this.errorBoolean = true;
+      return;
+    }
+    //#1 solution to alert about list element already existing and
+    //prevent from adding it again
+    // if(index !==  -1)
+    //   this.todoItem = "takie zadanie jest już dodane!";
+    // else{
+
+    //both solutions share this part
+      const item = { id: ++this.lastId, name: this.todoItem };
+      this.items.push(item);
+      this.todoItem = "";
+
+    //#2
+    this.errorBoolean = false;
   }
 
   deleteItem(id: number){
